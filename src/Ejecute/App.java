a8d6817b6ce24ae583b31463a605edd53b6f568b
GIGABYTE <GIGABYTE@DESKTOP-20IMHNT>
2022-04-03 10:58:56 -0500

    holaaa

--- src/Ejecute/App.java
+++ src/Ejecute/App.java
@@ -10,7 +10,7 @@ public class App {
     public static void main(String[] args) {
         ControllerViewLogin controller=new ControllerViewLogin();
         controller.run();
-        
+            ///holaa
         
         File archivoProductos=new File("archivoProductos.txt");
         if(archivoProductos.exists()){
