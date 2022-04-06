4aba1206d392307de24485fdf74ad1411260c874
GIGABYTE <GIGABYTE@DESKTOP-20IMHNT>
2022-04-05 21:01:40 -0500

    FILTRO LISTO

--- src/Ejecute/App.java
+++ src/Ejecute/App.java
@@ -10,7 +10,7 @@ public class App {
     public static void main(String[] args) {
         ControllerViewLogin controller=new ControllerViewLogin();
         controller.run();
-        
+        //xd
         File archivoProductos=new File("archivoProductos.txt");
         if(archivoProductos.exists()){
         try {
