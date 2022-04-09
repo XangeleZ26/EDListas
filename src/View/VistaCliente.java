ce1ed5ff3db7748eba0a341f606e9c3f49b39176
GIGABYTE <GIGABYTE@DESKTOP-20IMHNT>
2022-04-09 18:31:02 -0500

    pequeño avance para la vista Clientes (ya tengo una idea de como ir avanzando)

--- src/View/VistaCliente.java
+++ src/View/VistaCliente.java
@@ -2,6 +2,7 @@
 package View;
 
 import Model.Configuracion;
+import java.awt.Color;
 import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -9,54 +10,97 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import javax.swing.*;
 
 /**
  *
  * @author GIGABYTE
  */
 public class VistaCliente extends javax.swing.JFrame {
-
+    
     public VistaCliente() {
-//    JPanel panelProductos = new JPanel();
-//        panelProductos.setLayout(null);
+        
+        //modificacion de tamaño de JFrame
+        this.setExtendedState(MAXIMIZED_BOTH);
+        //modificacion de paneles
+        
+    //panel Background
+    JPanel panelBackground = new JPanel();
+    panelBackground.setLayout(null);
+    panelBackground.setBounds(0,0, 100, 800);
+    panelBackground.setBackground(Color.black);
+    //panel Banner
+    JPanel panelPresentacion=new JPanel();
+        panelPresentacion.setLayout(null);
+    panelPresentacion.setBounds(0,0, 1170, 100); 
+    panelPresentacion.setBackground(Color.white);
+    JLabel pruebaTitulo=new JLabel();
+    pruebaTitulo.setText("Esto es un titulo");
+    pruebaTitulo.setBounds(0, 50,1100,20);
+    pruebaTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+    panelPresentacion.add(pruebaTitulo);
+    
+    //panel Productos
+    JPanel panelProducto=new JPanel();
+     panelProducto.setLayout(null);
+     panelProducto.setBounds(33,150, 1070, 600);
+     panelProducto.setBackground(Color.white);
+     
+
+     JLabel xd=new JLabel();
+     xd.setText("hoaa");
+     xd.setBounds(55,170,100,20);
+     JLabel xd2=new JLabel();
+     xd2.setText("hoaa222");
+     xd2.setBounds(55,1000,100,20);
+     xd2.setForeground(Color.red);
+     panelProducto.add(xd);
+     panelProducto.add(xd2);
+     
+     JScrollPane scrollPane=new JScrollPane(panelProducto, 
+   ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,  
+   ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        
+    //Implementar paneles al panel background
+    panelBackground.add(panelPresentacion);
+    panelBackground.add(scrollPane);
+    //Inicio de colocacion de imagenes
 //        int left=0;
 //        int down=0;
-//        setSize(1100, 800);
-////        for (int i = 0; i < 5; i++) {
+//        for (int i = 0; i < 5; i++) {
 //            int leftDato=left;
 //            int downDato=down;
 //            JLabel imagenLabel = new JLabel();
 //            JLabel nombreLabel=new JLabel();
 //            JLabel precioLabel=new JLabel();
 //            
-//            imagenLabel.setIcon(scaleImage((ImageIcon) Configuracion.arrProductos.getArregloP()[0].getImagen(),170,170));
-//            nombreLabel.setText("pruebita");
+//            imagenLabel.setIcon(scaleImage((ImageIcon) Configuracion.arrProductos.getArregloP()[i].getImagen(),170,170));
+//            nombreLabel.setText(Configuracion.arrProductos.getArregloP()[i].getNombreProducto());
 //            nombreLabel.setHorizontalAlignment(SwingConstants.CENTER);
-//            precioLabel.setText("$50");
+//            precioLabel.setText("S/. "+String.valueOf(Configuracion.arrProductos.getArregloP()[i].getValorXUnidad()));
 //            precioLabel.setHorizontalAlignment(SwingConstants.CENTER);
+//            String dato=Configuracion.arrProductos.getArregloP()[i].getNombreProducto();
 //            imagenLabel.addMouseListener(new MouseAdapter() {
 //                @Override
 //                public void mouseClicked(MouseEvent e) {
-//                    System.out.println("XDDD perro ");
+//                    System.out.println(dato);
 //                    
 //                }
 //            });
 //
-//            imagenLabel.setBounds(20+leftDato, 90, 170,170);
-//            nombreLabel.setBounds(20+leftDato, 230, 150,20);
-//            precioLabel.setBounds(20+leftDato, 250,150,10);
+//            imagenLabel.setBounds(40+leftDato, 100, 170,170);
+//            nombreLabel.setBounds(40+leftDato, 245, 170,20);
+//            precioLabel.setBounds(40+leftDato, 265,170,10);
 //            panelProductos.add(imagenLabel);
 //            panelProductos.add(nombreLabel);
 //            panelProductos.add(precioLabel);
-//            left+=60;
-////        }
-//
-//        add(panelProductos);
-//        
-//        setVisible(true);
-//        setLocationRelativeTo(null);
-//        
+//            left+=200;
+//        }
+
+        add(panelBackground);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        
     }
 
 
